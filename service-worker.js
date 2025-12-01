b1a4fba5fc2343cdb7b676d44d8cc5303528492e
Jan Dedek <jan.dedek@futtec.cz>
2025-12-01 08:54:18 +0100

    start

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'foto-poznamky-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon2.jpg',
+  './icon512.png' 
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
