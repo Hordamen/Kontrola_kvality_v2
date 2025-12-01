9d174ca844de958237a1a5d917f05b08c1c6cbb4
Jan Dedek <jan.dedek@futtec.cz>
2025-12-01 11:41:46 +0100

    zase

--- service-worker.js
+++ service-worker.js
@@ -19,7 +19,13 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      );
+    }).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', event => {
