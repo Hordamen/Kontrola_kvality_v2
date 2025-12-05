c0b4c74b724ee4d68652ccdcf85a55b270f51807
Jan Dedek <jan.dedek@futtec.cz>
2025-12-05 13:46:01 +0100

    oprava mapy

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'foto-poznamky-cache-v2.2';
+const CACHE_NAME = 'foto-poznamky-cache-v2.2.1';
 const urlsToCache = [
   './',
   './index.html',
-  './manifest.json?v=2.2',
+  './manifest.json?v=2.2.1',
   './icon192.png',
   './icon512.png'
 ];
