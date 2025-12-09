754eb53b08de05c9288609ee079be5b8e82dde47
Jan Dedek <jan.dedek@futtec.cz>
2025-12-09 09:17:59 +0100

    přesun záznamů

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'foto-poznamky-cache-v2.2.8';
+const CACHE_NAME = 'foto-poznamky-cache-v2.2.9';
 const urlsToCache = [
   './',
   './index.html',
-  './manifest.json?v=2.2.8',
+  './manifest.json?v=2.2.9',
   './icon192.png',
   './icon512.png'
 ];
